b7d5a0cc98c19e47353e954d041f6dc9d27adbfa
freewind <nowindlee@gmail.com>
2018-07-21 17:42:43 +0800

    demo done

--- src/main/kotlin/example/Hello.kt
+++ src/main/kotlin/example/Hello.kt
@@ -2,10 +2,52 @@ package example
 
 import javafx.geometry.Pos
 import tornadofx.*
+import com.sun.javafx.robot.impl.FXRobotHelper.getChildren
+import javafx.geometry.Insets
+import javafx.scene.control.Label
+import javafx.scene.layout.Pane
+
 
 class HelloWorld : View() {
-    override val root = hbox {
-        label("Hello world")
+    override val root = gridpane {
+        row {
+            label("Username") {
+                useMaxWidth = true
+                gridpaneConstraints {
+                    marginBottom = 10.0
+                    columnSpan = 2
+                }
+            }
+            textfield()
+        }
+        row {
+            label("Password")
+            passwordfield { }
+        }
+        row {
+            label("")
+            button("Login") {
+                gridpaneConstraints {
+                    this.columnIndex = 3
+                }
+            }
+        }
+        gridpaneConstraints {
+            padding = Insets(10.0, 10.0, 10.0, 10.0)
+            hgap = 10.0
+            vgap = 10.0
+        }
+    }
+
+    private fun cell(label: String): Pane {
+        return object : Pane() {
+            init {
+                style = "-fx-background-color: RED"
+                prefWidth = 50.0
+                prefHeight = 50.0
+                children.add(Label(label))
+            }
+        }
     }
 }
 
@@ -14,8 +56,6 @@ class HelloWorldStyle : Stylesheet() {
         root {
             prefWidth = 400.px
             prefHeight = 400.px
-            alignment = Pos.CENTER
-            fontSize = 50.px
         }
     }
 }
